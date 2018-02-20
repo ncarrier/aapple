27c654b2ea1b10503d3ebb19615adbcd830bbdd4
Nicolas Carrier <carrier.nicolas0@gmail.com>
2018-02-20 05:10:58 +0100

    déplacement + labyrinthe

--- yw_maze.py
+++ yw_maze.py
@@ -4,17 +4,19 @@ from pygame import locals as pl
 
 pygame.init()
 
+X = 0
+Y = 1
 sol = 0
 mur = 1
 out = 2
 labyrinthe = [[mur, mur, mur, mur, mur, mur, mur, mur, mur, mur],
-              [mur, sol, sol, sol, sol, sol, sol, sol, out, mur],
-              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
-              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
-              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
-              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
-              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
-              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, sol, mur, sol, sol, sol, mur, mur, out, mur],
+              [mur, sol, mur, sol, mur, sol, mur, sol, sol, mur],
+              [mur, sol, sol, sol, mur, sol, mur, sol, mur, mur],
+              [mur, sol, mur, mur, mur, mur, mur, sol, sol, mur],
+              [mur, sol, sol, mur, sol, sol, sol, mur, sol, mur],
+              [mur, mur, sol, sol, sol, mur, sol, mur, sol, mur],
+              [mur, sol, mur, mur, mur, mur, sol, mur, sol, mur],
               [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
               [mur, mur, mur, mur, mur, mur, mur, mur, mur, mur]]
 
@@ -25,6 +27,7 @@ image_mur = pygame.image.load("./resources/64/mur_64.jpg").convert()
 image_sol = pygame.image.load("./resources/64/sol_64.jpg").convert()
 image_out = pygame.image.load("./resources/64/sortie_64.png").convert_alpha()
 image_heros = pygame.image.load("./resources/64/heros_64.png").convert_alpha()
+
 
 def affiche_labyrinthe(ecran, labyrinthe):
     for x in range(0, 10):
@@ -37,24 +40,39 @@ def affiche_labyrinthe(ecran, labyrinthe):
                 ecran.blit(image_sol, (x * 64, y * 64))
                 ecran.blit(image_out, (x * 64, y * 64))
 
+
+def affiche_heros(ecran, heros):
+    ecran.blit(image_heros, (heros[X] * 64, heros[Y] * 64))
+
+
+heros = [1, 1]
 boucle = True
 pause = False
 horloge = pygame.time.Clock()
 while boucle:
     horloge.tick(60)
     affiche_labyrinthe(ecran, labyrinthe)
+    affiche_heros(ecran, heros)
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
             boucle = False
         elif evenement.type == pygame.KEYDOWN:
             if evenement.key == pygame.K_DOWN:
-                print("haut")
+                heros[Y] += 1
+                if labyrinthe[heros[Y]][heros[X]] == mur:
+                    heros[Y] -= 1
             elif evenement.key == pygame.K_LEFT:
-                print("gauche")
+                heros[X] -= 1
+                if labyrinthe[heros[Y]][heros[X]] == mur:
+                    heros[X] += 1
             elif evenement.key == pygame.K_RIGHT:
-                print("right")
+                heros[X] += 1
+                if labyrinthe[heros[Y]][heros[X]] == mur:
+                    heros[X] -= 1
             elif evenement.key == pygame.K_UP:
-                print("haut")
+                heros[Y] -= 1
+                if labyrinthe[heros[Y]][heros[X]] == mur:
+                    heros[Y] += 1
             elif evenement.key == pygame.K_p:
                 print("PAUSE !")
                 pause = not pause
