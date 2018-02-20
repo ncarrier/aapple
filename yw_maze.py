e29f0763d8d49b28af80c7ad3116e0a382adf82a
Nicolas Carrier <carrier.nicolas0@gmail.com>
2018-02-20 04:49:48 +0100

    affichage du labyrinthe

--- yw_maze.py
+++ yw_maze.py
@@ -4,12 +4,45 @@ from pygame import locals as pl
 
 pygame.init()
 
+sol = 0
+mur = 1
+out = 2
+labyrinthe = [[mur, mur, mur, mur, mur, mur, mur, mur, mur, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, out, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, sol, sol, sol, sol, sol, sol, sol, sol, mur],
+              [mur, mur, mur, mur, mur, mur, mur, mur, mur, mur]]
+
 ecran = pygame.display.set_mode((640, 640))#, pl.FULLSCREEN)
 pygame.mouse.set_visible(False)
 
+image_mur = pygame.image.load("./resources/64/mur_64.jpg").convert()
+image_sol = pygame.image.load("./resources/64/sol_64.jpg").convert()
+image_out = pygame.image.load("./resources/64/sortie_64.png").convert_alpha()
+image_heros = pygame.image.load("./resources/64/heros_64.png").convert_alpha()
+
+def affiche_labyrinthe(ecran, labyrinthe):
+    for x in range(0, 10):
+        for y in range(0, 10):
+            if labyrinthe[y][x] == mur:
+                ecran.blit(image_mur, (x * 64, y * 64))
+            elif labyrinthe[y][x] == sol:
+                ecran.blit(image_sol, (x * 64, y * 64))
+            elif labyrinthe[y][x] == out:
+                ecran.blit(image_sol, (x * 64, y * 64))
+                ecran.blit(image_out, (x * 64, y * 64))
+
 boucle = True
 pause = False
+horloge = pygame.time.Clock()
 while boucle:
+    horloge.tick(60)
+    affiche_labyrinthe(ecran, labyrinthe)
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
             boucle = False
@@ -27,5 +60,6 @@ while boucle:
                 pause = not pause
             elif evenement.key == pygame.K_ESCAPE:
                 boucle = False
+    pygame.display.flip()
 
 pygame.quit()
